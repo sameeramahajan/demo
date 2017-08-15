be31b782309ac77b4115a2f8e85f25b0f6720f36
sameera <sameera.mahajan.85@gmail.com>
2017-08-15 21:39:42 +0530

    first commit

---
+++ src/main/java/com/demo/Demo.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.demo;
+
+/**
+ * @author sameera
+ *
+ */
+public class Demo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!!!....Sameeerrraaa");
+	}
+
+}
---
+++ src/test/java/com/demo/Demotest.java
@@ -0,0 +1,15 @@
+package com.demo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Demotest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+		System.out.println("testing");
+	}
+
+}
